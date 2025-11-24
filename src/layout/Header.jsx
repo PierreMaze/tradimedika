dc6ccec37763a742d49806e03f52df3c67dee73f
PierreMaze <promazelaygue@gmail.com>
2025-11-24 15:37:41 +0100

    Feature: Fix header with sticky effect.

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,36 @@
+import { motion } from "framer-motion";
 import DarkModeToggle from "../components/btn/DarkModeToggle";
 import LogoTradimedika from "./components/LogoTradimedika";
-const Header = () => {
-  return (
-    <header className="bg-light/80 flex h-20 w-full items-center justify-between border-b-2 border-dashed px-6 py-4 lg:w-2/3">
-      <LogoTradimedika />
-      <DarkModeToggle />
-    </header>
-  );
+
+const headerVariants = {
+  hidden: {
+    y: -100,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 20,
+      duration: 0.6,
+    },
+  },
 };
 
-export default Header;
+export default function Header() {
+  return (
+    <motion.header
+      className="bg-light sticky top-0 right-0 left-0 z-50 h-20 w-full border-b-2 border-dashed px-6 py-4 lg:w-2/3"
+      variants={headerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <div className="flex items-center justify-between lg:mx-8">
+        <LogoTradimedika />
+        <DarkModeToggle />
+      </div>
+    </motion.header>
+  );
+}
