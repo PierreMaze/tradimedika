d02776faba7fdebfc70ece3f9e126a8cf81e1829
PierreMaze Desktop <promazelaygue@gmail.com>
2025-12-02 12:29:03 +0100

    fix(Header): correct transition class for improved styling consistency

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
   const { isDarkMode } = useTheme();
   return (
     <motion.header
-      className={`border-4transition sticky top-0 right-0 left-0 z-50 h-20 w-full duration-300 ease-in-out ${isDarkMode ? "bg-dark" : "bg-light"}`}
+      className={`transition sticky top-0 right-0 left-0 z-50 h-20 w-full duration-300 ease-in-out ${isDarkMode ? "bg-dark" : "bg-light"}`}
       variants={headerVariants}
       initial="hidden"
       animate="visible"
@@ -33,7 +33,7 @@ export default function Header() {
       <div
         className={`border-out mx-auto flex min-h-full w-full items-center border-b-2 border-dashed transition duration-300 ease-in-out lg:w-3/4 2xl:w-2/3 ${isDarkMode ? "border-light/60" : "border-dark/80 "}`}
       >
-        <div className="flex h-auto w-full items-center justify-between  mx-4 lg:mx-8">
+        <div className="flex h-auto w-full items-center justify-between mx-4 lg:mx-8">
           <LogoTradimedika />
           <DarkModeToggle />
         </div>
