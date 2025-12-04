35f03442bf16de0cd5ee2f6b7377b08bdcda1ae2
PierreMaze <promazelaygue@gmail.com>
2025-12-04 11:41:34 +0100

    hotfix(Header.jsx): ajuster les classes CSS pour une meilleure mise en page et cohérence visuelle

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -25,15 +25,15 @@ export default function Header() {
   const { isDarkMode } = useTheme();
   return (
     <motion.header
-      className={`transition sticky top-0 right-0 left-0 z-50 h-20 w-full duration-300 ease-in-out ${isDarkMode ? "bg-dark" : "bg-light"}`}
+      className={`sticky top-0 right-0 left-0 z-50 h-auto w-full transition duration-300 ease-in-out ${isDarkMode ? "bg-dark" : "bg-light"}`}
       variants={headerVariants}
       initial="hidden"
       animate="visible"
     >
       <div
-        className={`border-out mx-auto flex min-h-full w-full items-center border-b-2 border-dashed transition duration-300 ease-in-out lg:w-3/4 2xl:w-2/3 ${isDarkMode ? "border-light/60" : "border-dark/80 "}`}
+        className={`border-out mx-auto flex h-20 w-full items-center border-b-2 border-dashed transition duration-300 ease-in-out lg:w-3/4 2xl:w-2/3 ${isDarkMode ? "border-light/60" : "border-dark/80 "}`}
       >
-        <div className="flex h-auto w-full items-center py-6 justify-between mx-4 lg:mx-8">
+        <div className="mx-4 flex w-full items-center justify-between py-6 lg:mx-8">
           <LogoTradimedika />
           <DarkModeToggle />
         </div>
