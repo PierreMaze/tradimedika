defb433da2890fd2e02d0f5bd870449bc7c5ee7a
PierreMaze Desktop <promazelaygue@gmail.com>
2026-02-15 16:08:37 +0100

    Updates

---
+++ assets/useConsent-C3BFtOqe.js
@@ -0,0 +1,1 @@
+import{r as e,al as n}from"./index-DLX45XN4.js";function r(){const t=e.useContext(n);if(!t)throw new Error("useConsent must be used within a ConsentProvider");return t}export{r as u};
